d1e7b230930f8d82fea756e8c2dc34eb95fc792e
8Root <106619963+8Root@users.noreply.github.com>
2023-01-18 18:41:10 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,17 +1,7 @@
-title Kill all running apps
-cd c:\windows\System32
-for /f "skip=3 tokens=1" %%i in ('TASKLIST /FI "USERNAME eq %userdomain%\%username%" /FI "STATUS eq running"') do (
-if not "%%i"=="svchost.exe" (
-if not "%%i"=="explorer.exe" (
-if not "%%i"=="cmd.exe" (
-if not "%%i"=="tasklist.exe" (
-echo.
-taskkill /f /im "%%i" 
-taskkill /f /im explorer.exe
-echo.
-)
-)
-)
-)
-)
-pause
+@echo off
+set INTERVAL=10
+:loop
+tasklist /fi "ImageName eq LeagueofLegends.exe" /fo csv 2>NUL | find /I "myapp.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /f /im explorer.exe
+timeout %INTERVAL%
+goto:loop
