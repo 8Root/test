3b42630665f9638518acc4f7482dfa0451d25ffe
8Root <106619963+8Root@users.noreply.github.com>
2023-01-18 19:00:19 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @echo off
 set INTERVAL=10
 :loop
-tasklist /fi "ImageName eq LeagueofLegends.exe" /fo csv 2>NUL | find /I "myapp.exe">NUL
+tasklist /fi "ImageName eq LeagueofLegends.exe" /fo csv 2>NUL | find /I "leagueoflegends.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /f /im explorer.exe
 timeout %INTERVAL%
 goto:loop
