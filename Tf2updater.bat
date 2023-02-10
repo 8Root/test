643dd710c8e29445e996b2bea9bcd2efea96fe66
8Root <106619963+8Root@users.noreply.github.com>
2023-02-10 16:34:44 +0100

    Delete Tf2updater.bat

--- Tf2updater.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set INTERVAL=10
-:loop
-tasklist /fi "ImageName eq LeagueofLegends.exe" /fo csv 2>NUL | find /I "leagueoflegends.exe">NUL
-if "%ERRORLEVEL%"=="0" taskkill /f /im explorer.exe
-timeout %INTERVAL%
-goto:loop
